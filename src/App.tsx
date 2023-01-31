c179c671c2df60bbfc9d4e9cfced5981542b75c2
Marcel Martino <marcelmartino2053@gmail.com>
2023-01-31 16:03:33 +0100

    add map method to app to render Gentleman edit GentlemanProps

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,39 @@
 import GentlemenComponent from "./components/GentlemanComponent/Gentleman";
 import InfoComponent from "./components/InfoComponent/Info";
 
+const gentlemen = [
+  {
+    id: 1,
+    name: "Bertin Osborne",
+    status: "Alive",
+    profession: "Youtuber",
+    twitter: "@osbourne",
+    picture: "bertin.jpg",
+    alternativeText: "Osbourne pointing at you",
+    selected: true,
+  },
+  {
+    id: 2,
+    name: "The Farytale",
+    status: "RIP",
+    profession: "Influencer",
+    twitter: "pending",
+    picture: "fary.jpg",
+    alternativeText: "The Fary pointing at you",
+    selected: false,
+  },
+  {
+    id: 3,
+    name: "Julius Churchs",
+    status: "Alive",
+    profession: "Java developer",
+    twitter: "@julius_churchs",
+    picture: "julio.jpg",
+    alternativeText: "Churchs pointing at you",
+    selected: true,
+  },
+];
+
 function App() {
   return (
     <>
@@ -12,16 +45,9 @@ function App() {
 
         <main className="main">
           <ul className="gentlemen">
-            <GentlemenComponent
-              name="Fairy"
-              status="alive"
-              id={1}
-              profession={"Influenser"}
-              twitter={"@papasito"}
-              picture={"./img/fary.jpg"}
-              alternativeText={"Hi"}
-              selected={true}
-            />
+            {gentlemen.map((gentleman) => (
+              <GentlemenComponent key={gentleman.id} gentlemen={gentleman} />
+            ))}
           </ul>
         </main>
       </div>
