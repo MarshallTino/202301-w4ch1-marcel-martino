df2e80f7439fcdb2481fd3a77c6fd98a4884e67c
Marcel Martino <marcelmartino2053@gmail.com>
2023-01-31 09:10:03 +0100

    remove gentlemanArray from appTSX fix favicon

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,6 @@ import GentlemenComponent from "./components/GentlemanComponent/Gentleman";
 import InfoComponent from "./components/InfoComponent/Info";
 
 function App() {
-  let gentlemanArray = [
-    {
-      id: 1,
-      name: "Bertin Osborne",
-      status: "Alive",
-      profession: "Youtuber",
-      twitter: "@osbourne",
-      picture: "bertin.jpg",
-      alternativeText: "Osbourne pointing at you",
-      selected: true,
-    },
-    {
-      id: 2,
-      name: "The Farytale",
-      status: "RIP",
-      profession: "Influencer",
-      twitter: "pending",
-      picture: "fary.jpg",
-      alternativeText: "The Fary pointing at you",
-      selected: false,
-    },
-    {
-      id: 3,
-      name: "Julius Churchs",
-      status: "Alive",
-      profession: "Java developer",
-      twitter: "@julius_churchs",
-      picture: "julio.jpg",
-      alternativeText: "Churchs pointing at you",
-      selected: true,
-    },
-  ];
-
   return (
     <>
       <div className="container">
